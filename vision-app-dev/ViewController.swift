635fc8471edc4c6f8dd483a68e2693c6da7f8ea6
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-12 13:09:56 -0500

    Initial Commit

---
+++ vision-app-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-app-dev
+//
+//  Created by Rick Pearce on 4/12/18.
+//  Copyright © 2018 Rick Pearce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
